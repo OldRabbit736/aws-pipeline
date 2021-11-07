6c9b318d6e6bd8c8f6f395d98819f66b2b5a6d95
jspark <sylvan0212@gmail.com>
2021-11-07 20:12:51 +0900

    Add test for addServiceStage method of PipelineStack

--- test/pipeline.test.ts
+++ test/pipeline.test.ts
@@ -3,9 +3,14 @@ import {
   matchTemplate,
   MatchStyle,
   SynthUtils,
+  haveResourceLike,
+  arrayWith,
+  objectLike,
 } from "@aws-cdk/assert";
 import * as cdk from "@aws-cdk/core";
+import { App } from "@aws-cdk/core";
 import * as Pipeline from "../lib/pipeline-stack";
+import { ServiceStack } from "../lib/service-stack";
 
 test("Empty Stack", () => {
   const app = new cdk.App();
@@ -14,4 +19,25 @@ test("Empty Stack", () => {
   // THEN
 
   expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
+});
+
+test("Adding service stage", () => {
+  // GIVEN
+  const app = new App();
+  const serviceStack = new ServiceStack(app, "ServiceStack");
+  const pipelineStack = new Pipeline.PipelineStack(app, "PipelineStack");
+
+  // WHEN
+  pipelineStack.addServiceStage(serviceStack, "TestStage");
+
+  // THEN
+  expectCDK(pipelineStack).to(
+    haveResourceLike("AWS::CodePipeline::Pipeline", {
+      Stages: arrayWith(
+        objectLike({
+          Name: "TestStage",
+        })
+      ),
+    })
+  );
 });
