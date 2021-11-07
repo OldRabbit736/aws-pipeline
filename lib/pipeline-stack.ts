2a51759c17d109a9b05ae302332e91fe4b673e30
jspark <sylvan0212@gmail.com>
2021-11-07 10:55:27 +0900

    Add service source action to the pipeline

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -22,7 +22,8 @@ export class PipelineStack extends cdk.Stack {
       // restartExecutionOnUpdate: false,
     });
 
-    const sourceOutput = new Artifact("SourceOutput");
+    const cdkSourceOutput = new Artifact("CDKSourceOutput");
+    const serviceSourceOutput = new Artifact("ServiceSourceOutput");
 
     pipeline.addStage({
       stageName: "Source",
@@ -33,7 +34,15 @@ export class PipelineStack extends cdk.Stack {
           branch: "main",
           actionName: "Pipeline_Source",
           oauthToken: SecretValue.secretsManager("github-pipeline-token"),
-          output: sourceOutput,
+          output: cdkSourceOutput,
+        }),
+        new GitHubSourceAction({
+          owner: "OldRabbit736",
+          repo: "express-lambda",
+          branch: "main",
+          actionName: "Service_Source",
+          oauthToken: SecretValue.secretsManager("github-pipeline-token"),
+          output: serviceSourceOutput,
         }),
       ],
     });
@@ -45,7 +54,7 @@ export class PipelineStack extends cdk.Stack {
       actions: [
         new CodeBuildAction({
           actionName: "CDK_Build",
-          input: sourceOutput,
+          input: cdkSourceOutput,
           outputs: [cdkBuildOutput],
           project: new PipelineProject(this, "CdkBuildProject", {
             environment: {
