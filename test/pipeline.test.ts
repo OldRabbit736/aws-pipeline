e9a6c96cfac12d2d3791a968e0f531d3c5dd81eb
jspark <sylvan0212@gmail.com>
2021-11-07 20:32:38 +0900

    Add billing update action to prod stage

--- test/pipeline.test.ts
+++ test/pipeline.test.ts
@@ -9,6 +9,7 @@ import {
 } from "@aws-cdk/assert";
 import * as cdk from "@aws-cdk/core";
 import { App } from "@aws-cdk/core";
+import { BillingStack } from "../lib/billing-stack";
 import * as Pipeline from "../lib/pipeline-stack";
 import { ServiceStack } from "../lib/service-stack";
 
@@ -36,6 +37,36 @@ test("Adding service stage", () => {
       Stages: arrayWith(
         objectLike({
           Name: "TestStage",
+        })
+      ),
+    })
+  );
+});
+
+test("Adding billing stack to a stage", () => {
+  // GIVEN
+  const app = new App();
+  const serviceStack = new ServiceStack(app, "ServiceStack");
+  const pipelineStack = new Pipeline.PipelineStack(app, "PipelineStack");
+  const billingStack = new BillingStack(app, "BillingStack", {
+    budgetAmount: 5,
+    emailAddress: "test@example.com",
+  });
+  const testStage = pipelineStack.addServiceStage(serviceStack, "TestStage");
+
+  // WHEN
+  pipelineStack.addBillingStackToStage(billingStack, testStage);
+
+  // THEN
+  expectCDK(pipelineStack).to(
+    haveResourceLike("AWS::CodePipeline::Pipeline", {
+      Stages: arrayWith(
+        objectLike({
+          Actions: arrayWith(
+            objectLike({
+              Name: "Billing_Update",
+            })
+          ),
         })
       ),
     })
